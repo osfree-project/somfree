6b78ebb5ba3a0ebaae5edd077460ca63fc8a674d
somtoolkit <somtoolkit@c883dd1d-f74d-40fc-8d12-59f342f64db7>
2020-06-07 13:12:06 +0000

    From Windows 10 IoT
    git-svn-id: https://svn.code.sf.net/p/somfree/code/trunk@49 c883dd1d-f74d-40fc-8d12-59f342f64db7

--- ipv6test/win32arm/ipv6test.h
+++ ipv6test/win32arm/ipv6test.h
@@ -1,5 +1,5 @@
-/* Generated on CPU_VENDOR_OS=i386-unknown-win32 */
-/* Sun Jun  7 10:49:56 2020 */
-#define IPV6_WILL_BIND 50607
-/* connect(AF_INET4,127.0.0.1:50607) failed */
-#define IPV6_AND_IPV4_BIND 50609
+/* Generated on CPU_VENDOR_OS=arm-unknown-win32 */
+/* Sun Jun  7 14:58:24 2020 */
+#define IPV6_WILL_BIND 49718
+/* connect(AF_INET4,127.0.0.1:49718) failed */
+#define IPV6_AND_IPV4_BIND 49720
