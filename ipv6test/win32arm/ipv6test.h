50c881038a04015efa523efc0b65236e00f5af91
somtoolkit <somtoolkit@c883dd1d-f74d-40fc-8d12-59f342f64db7>
2020-06-07 13:07:18 +0000

    IPv6 configuration for windows
    git-svn-id: https://svn.code.sf.net/p/somfree/code/trunk@48 c883dd1d-f74d-40fc-8d12-59f342f64db7

---
+++ ipv6test/win32arm/ipv6test.h
@@ -0,0 +1,5 @@
+/* Generated on CPU_VENDOR_OS=i386-unknown-win32 */
+/* Sun Jun  7 10:49:56 2020 */
+#define IPV6_WILL_BIND 50607
+/* connect(AF_INET4,127.0.0.1:50607) failed */
+#define IPV6_AND_IPV4_BIND 50609
---
+++ ipv6test/win64amd/ipv6test.h
@@ -0,0 +1,5 @@
+/* Generated on CPU_VENDOR_OS=x64-unknown-win64 */
+/* Sun Jun  7 11:32:03 2020 */
+#define IPV6_WILL_BIND 50749
+/* connect(AF_INET4,127.0.0.1:50749) failed */
+#define IPV6_AND_IPV4_BIND 50751
